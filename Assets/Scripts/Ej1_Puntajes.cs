e5c5de7ae5aaf3271944d2f7743fe39048e598e5
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-08-19 10:41:35 -0300

    19/8

---
+++ Assets/Scripts/Ej1_Puntajes.cs
@@ -0,0 +1,74 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1_Puntajes : MonoBehaviour
+{
+    //Se ve pero no se puede cambiar dentro del juego.
+    [SerializeField]int[] puntajes = new int[10];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("El mayor puntaje es "+ObtenerMayorPuntaje());
+        Debug.Log("El nivel con menor puntaje es "+ObtenerNivelDeMenorPuntaje());
+        Debug.Log("El promedio es " + ObtenerPuntajePromedio());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+        int mayorPuntaje = 0;
+        
+        //Foreach lo que hace es crear una variable auxiliar para ALMACENAR el valor de cada posicion del array.
+        foreach(int puntaje in puntajes)
+        {
+            if(puntaje > mayorPuntaje)
+            {
+                mayorPuntaje = puntaje;
+            }
+
+        }
+
+        return (mayorPuntaje);
+    }
+
+
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int indiceNivelDeMenorPuntaje = 0;
+        int menorPuntaje = puntajes[0];
+        //Foreach lo que hace es crear una variable auxiliar para ALMACENAR el valor de cada posicion del array.
+        for (int i = 1; i < puntajes.Length; i++)
+        {
+            if (puntajes[i] < menorPuntaje)
+            {
+                menorPuntaje = puntajes[i];
+                indiceNivelDeMenorPuntaje = i;
+            }
+        }
+        return (indiceNivelDeMenorPuntaje);
+    }
+
+    float ObtenerPuntajePromedio()
+    {
+        float promedio = 0;
+        float suma = 0;
+
+        foreach(int puntaje in puntajes)
+        {
+            suma =+ puntaje;
+         
+        }
+
+        promedio = suma/puntajes.Length;
+
+        return (promedio);
+    }
+}
---
+++ Assets/Scripts/Ej2_Dias_Meses.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ej2_Dias_Meses : MonoBehaviour
+{
+
+    [SerializeField] string[] nombresMeses = new string[12];
+    [SerializeField] int[] diasMeses = new int[12];
+    public string fechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValidarFormatoFecha(fechaIngresada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* La regla es que sea un string de 4 caracters donde
+     los dos primeros caracteres sean un número
+     entre 01 y 31 y los dos últimos un número entre 01 y 12.*/
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool fechaValida = false;
+
+        if(fecha.Length == 4)
+        {
+            string dia = "";
+            string mes = "";
+            dia += fecha[0];
+            dia += fecha[1];
+            int diaNumber = int.Parse(dia);
+            mes += fecha[2];
+            mes += fecha[3];
+            int mesNumber = int.Parse(mes);
+            if( diaNumber > 0 && diaNumber <= 31 && mesNumber > 0 && mesNumber >=12)
+            {
+                fechaValida=true;
+            }
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+
+        return (fechaValida);
+    }
+}
